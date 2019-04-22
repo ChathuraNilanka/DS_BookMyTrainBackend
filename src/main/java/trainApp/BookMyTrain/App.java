1e725ee68b6fe5e98ea04a083175ec388e5c6d95
unknown <chathuranilanka1024@gmail.com>
2019-04-22 14:33:15 +0530

    Initial Commit

---
+++ src/main/java/trainApp/BookMyTrain/App.java
@@ -0,0 +1,13 @@
+package trainApp.BookMyTrain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
